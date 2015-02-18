9b2b63d800c0af5d1cb7f4a2d1fcd405864646b1
Fabian Rinaldi <code.optica@gmail.com>
2015-02-19 07:47:16 +0800

    initial commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,44 @@
+class DemoController < ApplicationController
+  def initialize
+      @expense_controller = ExpenseController.new
+      @result = ""
+      @range_date_result = ""
+      @start_date_result = ""
+      @end_date_result = ""
+      @no_date_result = ""
+  end
+
+  def index
+    # @result
+    # @range_date_result
+    # @start_date_result
+    # @end_date_result
+    # @no_date_result
+    # @result = @expense_controller.get_expenses
+  end
+
+  def generate_create_expense_result
+    @result = @expense_controller.create_expense
+    render action: "index"
+  end
+
+  def generate_expense_date_range
+      @range_date_result = @expense_controller.get_expenses_date_range
+      render action: "index"
+  end
+
+  def generate_expense_start_date
+      @start_date_result = @expense_controller.get_expenses_start_date
+      render action: "index"
+  end
+
+   def generate_expense_end_date
+      @end_date_result = @expense_controller.get_expenses_end_date
+      render action: "index"
+  end
+
+  def generate_expense_no_date
+      @no_date_result = @expense_controller.get_expenses_no_date
+      render action: "index"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,58 @@
+Rails.application.routes.draw do
+   get 'demo/create_expense_result' => 'demo#generate_create_expense_result'
+  get 'demo/expense_date_range' => 'demo#generate_expense_date_range'
+  get 'demo/expense_start_date' => 'demo#generate_expense_start_date'
+  get 'demo/expense_end_date' => 'demo#generate_expense_end_date'
+  get 'demo/expense_no_date' => 'demo#generate_expense_no_date'
+  # You can have the root of your site routed with "root"
+   root 'demo#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
